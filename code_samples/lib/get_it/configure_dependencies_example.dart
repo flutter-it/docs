b3cd97790a708adbb41d9386bdc47bf98a0be823
Thomas Burkhart <thomas@burkharts.net>
2025-10-31 16:13:50 -0500

    Include GetIt instance declaration in configure_dependencies example
    Move 'final getIt = GetIt.instance;' inside the #region example block
    so it appears in the documentation. This is the critical first step that
    shows users how to create the global GetIt instance.

--- code_samples/lib/get_it/configure_dependencies_example.dart
+++ code_samples/lib/get_it/configure_dependencies_example.dart
@@ -1,10 +1,10 @@
 import 'package:get_it/get_it.dart';
 import '_shared/stubs.dart';
 
-final getIt = GetIt.instance;
-
 // #region example
 // lib/service_locator.dart
+
+final getIt = GetIt.instance;
 
 void configureDependencies() {
   // Register your services
