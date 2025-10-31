200d0c820a1a240af413b43dc336d419628f3ebe
Thomas Burkhart <thomas@burkharts.net>
2025-10-30 21:20:18 -0500

    Revert wrongly classified signature files and add them to exclusion list

--- code_samples/lib/get_it/code_sample_3c73f756.dart
+++ code_samples/lib/get_it/code_sample_3c73f756.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: missing_function_body, unused_element
 bool checkLazySingletonInstanceExists<T>({
   String? instanceName,
-}) =>
-    false;
+})
--- code_samples/lib/get_it/code_sample_f4194899.dart
+++ code_samples/lib/get_it/code_sample_f4194899.dart
@@ -1,8 +1,5 @@
 // ignore_for_file: missing_function_body, unused_element
-class ObjectRegistration {}
-
 ObjectRegistration? findFirstObjectRegistration<T>({
   Object? instance,
   String? instanceName,
-}) =>
-    null;
+})
