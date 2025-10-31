1994d08b6adf3ec6077cbad53c06f327c16c0264
Thomas Burkhart <thomas@burkharts.net>
2025-10-30 21:25:11 -0500

    Fix __type_registration_example: Add _ObjectRegistration class and LinkedHashMap import

--- code_samples/lib/get_it/__type_registration_example.dart
+++ code_samples/lib/get_it/__type_registration_example.dart
@@ -1,9 +1,12 @@
 import 'package:get_it/get_it.dart';
-import '_shared/stubs.dart';
+import 'dart:collection';
 
 // #region example
+class _ObjectRegistration {}
+
 class _TypeRegistration<T> {
-  final registrations = <_ObjectRegistration>[];           // Unnamed registrations
-  final namedRegistrations = LinkedHashMap<String, ...>(); // Named registrations
+  final registrations = <_ObjectRegistration>[]; // Unnamed registrations
+  final namedRegistrations =
+      LinkedHashMap<String, _ObjectRegistration>(); // Named registrations
 }
 // #endregion example
