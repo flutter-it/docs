8db41eceaa6218e3256ba8e4ed2908bb6bed3961
Thomas Burkhart <thomas@burkharts.net>
2025-11-19 20:56:22 -0500

    Updated code samples

--- code_samples/lib/watch_it/debugging_patterns.dart
+++ code_samples/lib/watch_it/debugging_patterns.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: unused_local_variable, unreachable_from_main, undefined_class, unused_element, dead_code
+// ignore_for_file: unused_local_variable, unused_element, avoid_print
 import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 import '_shared/stubs.dart';
 
 final di = GetIt.instance;
-
-// NOTE: This file contains GOOD debugging examples only.
-// BAD examples that demonstrate errors are left inline in the documentation.
 
 // #region track_rebuild_frequency
 class TrackRebuildFrequency extends WatchingWidget {
@@ -14,28 +11,26 @@ class TrackRebuildFrequency extends WatchingWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('TodoList rebuild #${++_buildCount} at ${DateTime.now()}');
-
     final todos = watchValue((TodoManager m) => m.todos);
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (context, index) => Text(todos[index].title),
-    );
+
+    _buildCount++;
+    print('MyWidget rebuilt $_buildCount times');
+
+    return Text('Todo count: ${todos.length}');
   }
 }
 // #endregion track_rebuild_frequency
 
 // #region isolate_problem
-// Minimal test widget
-class IsolateProblemDebugWidget extends WatchingWidget {
+class IsolateProblem extends WatchingWidget {
   @override
   Widget build(BuildContext context) {
-    print('DebugWidget.build()');
+    // Minimal example - just the watch and rebuild
+    final value = watchValue((CounterManager m) => m.count);
 
-    final data = watchValue((DataManager m) => m.data);
-    print('Data value: $data');
+    print('Rebuild with value: $value');
 
-    return Text('Data: $data');
+    return Text('Value: $value');
   }
 }
 // #endregion isolate_problem
@@ -44,139 +39,24 @@ class IsolateProblemDebugWidget extends WatchingWidget {
 class MeasureRebuildCost extends WatchingWidget {
   @override
   Widget build(BuildContext context) {
-    final stopwatch = Stopwatch()..start();
+    final start = DateTime.now();
 
-    final data = watchValue((DataManager m) => m.data);
-    // ... build UI
+    final todos = watchValue((TodoManager m) => m.todos);
+    final user = watchValue((UserManager m) => m.currentUser);
+    final settings = watchIt<SettingsModel>();
 
-    stopwatch.stop();
-    print('ExpensiveWidget.build() took ${stopwatch.elapsedMicroseconds}μs');
+    // ... expensive widget tree ...
 
-    return Text(data);
+    final elapsed = DateTime.now().difference(start);
+    print('Build took: ${elapsed.inMilliseconds}ms');
+
+    return Column(
+      children: [
+        Text('Todos: ${todos.length}'),
+        Text('User: ${user?.name ?? "None"}'),
+        Text('Dark mode: ${settings.darkMode}'),
+      ],
+    );
   }
 }
 // #endregion measure_rebuild_cost
-
-// #region profile_memory
-class MemoryProfiler {
-  static void logCurrentUsage(String label) {
-    // In real app, use dart:developer
-    print('[$label] Memory check');
-  }
-}
-
-class ProfileMemoryWidget extends WatchingWidget {
-  @override
-  Widget build(BuildContext context) {
-    MemoryProfiler.logCurrentUsage('TodoList.build');
-
-    final todos = watchValue((TodoManager m) => m.todos);
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (context, index) => Text(todos[index].title),
-    );
-  }
-}
-// #endregion profile_memory
-
-// #region find_excessive_watch_calls
-class FindExcessiveWatchCalls extends WatchingWidget {
-  static final _watchCalls = <String, int>{};
-
-  T _profiledWatch<T>(String label, T Function() fn) {
-    _watchCalls[label] = (_watchCalls[label] ?? 0) + 1;
-    if (_watchCalls[label]! % 100 == 0) {
-      print('$label called ${_watchCalls[label]} times');
-    }
-    return fn();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final todos = _profiledWatch(
-      'todos',
-      () => watchValue((TodoManager m) => m.todos),
-    );
-
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (context, index) => Text(todos[index].title),
-    );
-  }
-}
-// #endregion find_excessive_watch_calls
-
-// #region custom_watch_wrapper_logging
-extension WatchDebug on WatchingWidget {
-  T debugWatch<T>(
-    String label,
-    T Function() watchFn,
-  ) {
-    print('[WATCH] $label - subscribing');
-    final result = watchFn();
-    print('[WATCH] $label - value: $result');
-    return result;
-  }
-}
-
-class CustomWatchWrapperWidget extends WatchingWidget {
-  @override
-  Widget build(BuildContext context) {
-    final todos = debugWatch(
-      'todos',
-      () => watchValue((TodoManager m) => m.todos),
-    );
-
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (context, index) => Text(todos[index].title),
-    );
-  }
-}
-// #endregion custom_watch_wrapper_logging
-
-// #region conditional_tracing
-class ConditionalTracingWidget extends WatchingWidget {
-  final bool enableTracing;
-
-  ConditionalTracingWidget({this.enableTracing = false});
-
-  @override
-  Widget build(BuildContext context) {
-    if (enableTracing) {
-      print('Building $runtimeType');
-    }
-
-    final data = watchValue((DataManager m) => m.data);
-
-    if (enableTracing) {
-      print('Data value: $data');
-    }
-
-    return Text('$data');
-  }
-}
-// #endregion conditional_tracing
-
-// #region detect_watch_ordering_violations
-class StrictWatchingWidget extends WatchingWidget {
-  final _watchLabels = <String>[];
-
-  T _strictWatch<T>(String label, T Function() fn) {
-    if (_watchLabels.contains(label)) {
-      throw StateError('Duplicate watch call: $label');
-    }
-    _watchLabels.add(label);
-    return fn();
-  }
-
-  // Use in subclasses:
-  // final todos = _strictWatch('todos', () => watchValue(...));
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-// #endregion detect_watch_ordering_violations
-
-void main() {}
